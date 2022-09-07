82e14b674bb7731064dfea181f977b0f137d72f1
malli <malli@LAPTOP-JKHBE8P5>
2022-09-07 13:23:33 +0530

    commit 2

---
+++ TwoTable/TwoTable.java
@@ -0,0 +1,14 @@
+public class TwoTable
+{
+	public static void main(String args[])
+	{
+	
+    	int x=2;
+	    for(int i=1;i<=10;i++)
+		{ 
+	    	int y=x*i;
+			System.out.println(y);
+		}
+	 System.out.println("End of the table");
+	}
+}
