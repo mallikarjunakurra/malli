81d1bde408271e43923453ffd45213defe083216
mallikarjunakurra <113004589+mallikarjunakurra@users.noreply.github.com>
2022-12-26 16:36:37 +0530

    Delete Project-1 directory

--- Project-1/src/main/java/com/springboothiber/dto/CourseDTO.java
+++
@@ -1,5 +0,0 @@
-package com.springboothiber.dto;
-
-public class CourseDTO {
-
-}
--- Project-1/src/main/java/com/springboothiber/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboothiber.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboothiber.entities.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-
-}
