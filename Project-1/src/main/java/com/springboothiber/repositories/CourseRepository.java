d62f57887b27a3b489bd8abdc48a008bf7adfdfd
mallikarjunakurra <113004589+mallikarjunakurra@users.noreply.github.com>
2022-12-29 23:40:12 +0530

    Delete Project-1 directory

--- Project-1/src/main/java/com/springboothiber/repositories/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboothiber.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springboothiber.entities.Courses;
-
-public interface CourseRepository extends JpaRepository<Courses, Integer>{
-
-}
